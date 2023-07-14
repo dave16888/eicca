84af5ec6f9c74be87d93aa95fcd394c321940dff
陳冠宏 <dave.chen@iisigroup.com>
2023-07-14 14:59:55 +0800

    1

--- assets/Default-10ffb010.js
+++
@@ -1,1 +0,0 @@
-import{V as c}from"./VMain-18346e87.js";import{r as p,o as e,c as t,w as r,a,V as u}from"./index-58dcbdd9.js";const l={__name:"View",setup(o){return(_,n)=>{const s=p("router-view");return e(),t(c,null,{default:r(()=>[a(s)]),_:1})}}},f={__name:"Default",setup(o){return(_,n)=>(e(),t(u,null,{default:r(()=>[a(l)]),_:1}))}};export{f as default};
